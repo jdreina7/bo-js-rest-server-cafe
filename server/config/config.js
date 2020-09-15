64478993071a07fa5d7599cb260faa8766e60300
jdreinar <jdreinar@hotmail.com>
2020-09-15 18:52:47 -0500

    DB_URI en el config

--- server/config/config.js
+++ server/config/config.js
@@ -19,3 +19,22 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
  ***********************/
 process.env.SEED = process.env.SEED || 'september-charglie@-12345';
 
+
+/***********************
+ * ENVIRONMENT
+ ***********************/
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+/***********************
+ * DB_URI
+ ***********************/
+let urlDB;
+
+if ( process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://admin:admin@cafedb.4ilhe.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -23,11 +23,10 @@ app.use(bodyParser.json());
 app.use( require('./routes/index') );
 
 
-mongoose.connect('mongodb://localhost:27017/cafe', {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
-// mongoose.connect('mongodb+srv://admin:admin@cafedb.4ilhe.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
+mongoose.connect( process.env.URLDB , {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
   if (err) throw err;
 
-  console.log('DB ONLINE!!!');
+  console.log('DB ONLINE ON', process.env.NODE_ENV );
 });
 
 
