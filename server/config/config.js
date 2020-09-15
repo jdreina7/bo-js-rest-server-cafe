49d778c3c0182042cbd2b9dc66587f9387dc25be
jdreinar <jdreinar@hotmail.com>
2020-09-15 18:59:00 -0500

    DB_URI en el config

--- server/config/config.js
+++ server/config/config.js
@@ -34,7 +34,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:admin@cafedb.4ilhe.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
